1841b3ab080460b833a89ec9c46477deb9bacc6f
LucasAUPOIL <lucas.aupoil@gmail.com>
2020-05-25 16:09:09 +0200

    Initial commit

---
+++ src/gameOfLife/controller.js
@@ -0,0 +1,13 @@
+export const controller = model => {
+  document.getElementById("start").addEventListener("click", () => {
+    model.run();
+  });
+
+  document.getElementById("stop").addEventListener("click", () => {
+    model.stop();
+  });
+
+  document.getElementById("reset").addEventListener("click", () => {
+    model.reset();
+  });
+};
